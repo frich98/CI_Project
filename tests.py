20d12fa24ba10fc4e696851c2a06c2d6e25f2a6c
TriWesley <38091374+TriWesley@users.noreply.github.com>
2020-08-04 23:38:21 -0400

    Function3 first commit - edit tests.py and added task.py

--- tests.py
+++ tests.py
@@ -1,10 +1,19 @@
 import unittest
+import random
+from task import conv_endian
 
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    # Testing Big Endian
+    def test1_function3(self):
+        for i in range(0, 1000):
+            decimal = random.randint(1, 999999)
+            big_hex = hex(decimal)[2:].upper()
+            if len(big_hex) % 2 == 1:
+                big_hex = '0' + big_hex
+            big_hex = ' '.join(big_hex[i:i + 2] for i in range(0, len(big_hex), 2))
+            self.assertEqual(conv_endian(decimal, 'big'), big_hex)
 
 
 if __name__ == '__main__':
