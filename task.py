44811934fe768c6d73f78f9053d5a92321ec1c3d
Bryce Hickson <45865175+hicksonb@users.noreply.github.com>
2020-08-08 00:33:58 -0500

    Added conv_num function and tests

--- task.py
+++ task.py
@@ -1,3 +1,93 @@
+def conv_num(num_str):
+    # If num_str is not a string
+    if not type(num_str) == str:
+        return None
+
+    # If num_str doesn't have at least length 1
+    if len(num_str) <= 0:
+        return None
+
+    neg_num = False
+    hex_num = False
+    float_num = False
+
+    # See if negative
+    if num_str[0] == "-":
+        neg_num = True
+
+    # See if hexadecimal
+    if not neg_num:
+        if num_str[0:2].lower() == "0x":
+            hex_num = True
+    else:
+        if num_str[1:3].lower() == "0x":
+            hex_num = True
+
+    # See if float
+    if "." in num_str:
+        float_num = True
+
+    # Set index for positive or negative
+    start_index = 0
+    if neg_num:
+        start_index = 1
+
+    # If hexadecimal
+    valid_hex_chars = "0123456789ABCDEF"
+    valid_hex_letters = "ABCDEF"
+    if hex_num:
+        start_index += 2
+        hex_str = num_str.upper()
+        hex_value = 0
+        n = 0
+        for i in range(len(hex_str) - 1, start_index - 1, -1):
+            if not hex_str[i] in valid_hex_chars:
+                return None
+            else:
+                if hex_str[i] in valid_hex_letters:
+                    index_value = ord(hex_str[i]) - 55
+                else:
+                    index_value = ord(hex_str[i]) - 48
+                hex_value += index_value * (16 ** n)
+                n += 1
+        if neg_num:
+            hex_value *= -1
+        return hex_value
+
+    # If float
+    if float_num:
+        float_value = 0.0
+        dec_index = num_str.find(".")
+        n = 0
+        for i in range(dec_index - 1, start_index - 1, -1):
+            if not num_str[i].isdigit():
+                return None
+            float_value += ((ord(num_str[i]) - 48) * (10 ** n))
+            n += 1
+        n = -1
+        for j in range(dec_index + 1, len(num_str)):
+            if not num_str[j].isdigit():
+                return None
+            float_value += ((ord(num_str[j]) - 48) * (10 ** (n)))
+            n -= 1
+        if neg_num:
+            float_value *= -1
+        return float_value
+
+    # If integer
+    int_value = 0
+    n = 0
+    for i in range(len(num_str) - 1, start_index - 1, -1):
+        if not num_str[i].isdigit():
+            return None
+        else:
+            int_value += ((ord(num_str[i]) - 48) * (10 ** n))
+            n += 1
+    if neg_num:
+        int_value *= -1
+    return int_value
+
+
 def conv_endian(num, endian='big'):
     """Accepts an int value as num, converts and returns it as a
     hexadecimal string"""
--- tests.py
+++ tests.py
@@ -4,6 +4,115 @@ from task import conv_endian
 
 
 class Test(unittest.TestCase):
+    # Testing Convert Number
+    def test01_function1(self):
+        num_str = "12345"
+        expected = 12345
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test02_function1(self):
+        num_str = "-12345"
+        expected = -12345
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test03_function1(self):
+        num_str = "-123.45"
+        expected = -123.45
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test04_function1(self):
+        num_str = ".45"
+        expected = 0.45
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test05_function1(self):
+        num_str = "-.45"
+        expected = -0.45
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test06_function1(self):
+        num_str = "123."
+        expected = 123.0
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test07_function1(self):
+        num_str = "-123."
+        expected = -123.0
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test08_function1(self):
+        num_str = "0xAD4"
+        expected = 2772
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test09_function1(self):
+        num_str = "0XAD4"
+        expected = 2772
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test10_function1(self):
+        num_str = "0xad4"
+        expected = 2772
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test11_function1(self):
+        num_str = "0XaD4"
+        expected = 2772
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test12_function1(self):
+        num_str = "-0xAD4"
+        expected = -2772
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test13_function1(self):
+        num_str = "12.3.45"
+        expected = None
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test14_function1(self):
+        num_str = "0xAZ4"
+        expected = None
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test15_function1(self):
+        num_str = "12345A"
+        expected = None
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test16_function1(self):
+        num_str = ""
+        expected = None
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test17_function1(self):
+        num_str = 12345
+        expected = None
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
+    def test18_function1(self):
+        num_str = 1.2
+        expected = None
+        self.assertEqual(conv_num(num_str), expected)
+        self.assertEqual(type(conv_num(num_str)), type(expected))
+
     # Testing Big Endian
     def test1_function3(self):
         for i in range(0, 1000):
